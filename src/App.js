3ff71a2e6faab3e5a2e0639382399affd89bde62
erangajayapathma <eranga987@gmail.com>
2021-07-10 18:11:54 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import { ChatEngine, ChatFeed } from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './components/ChatFeed';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return(
+        <ChatEngine
+            height="100vh"
+            projectID="dd504936-e3e4-4c3b-8f46-9ef16d64b973"
+            userName="eranga987@gmail.com"
+            userSecret="Amila123456789"
+            renderChatFeed={(chatAppProps) => <ChatFeed { ...chatAppProps}/>}
+
+        />
+    );
 }
 
 export default App;
---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,15 @@
+
+
+const ChatFeed = (props) => {
+    console.log(props);
+
+
+    return(
+        <div>
+            ChatFeed
+        </div>
+    );
+
+}
+
+export default ChatFeed;
